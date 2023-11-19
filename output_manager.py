a10a1c588d46d61318e8ddd1253a69777a21bab7
Mohammad Ghafourian-Nasiri <mgh.nasiri@gmail.com>
2023-11-19 17:57:02 -0500

    modified:   __pycache__/output_manager.cpython-310.pyc 	modified:   output_manager.py

--- output_manager.py
+++ output_manager.py
@@ -8,10 +8,25 @@ import matplotlib.pyplot as plt
 
 # Function for visualizing the graph
 def visualize_graph(G,depot,nx,x,my_pos):
-    tour_edges = [ e for e in G.edges if x[e].x > 0.9 ]
+    # Create a figure and axis object
+
+    fig, ax = plt.subplots()
+
+    tour_edges = [e for e in G.edges if x[e].x > 0.9]
     node_colors = ["red" if node == depot else "blue" for node in G.nodes()]
     node_sizes = [200 if node == depot else 100 for node in G.nodes()]
+    
+    # Counting nodes and edges
+    num_nodes = len(G.nodes())
+    num_edges = len(tour_edges)
 
-    nx.draw(G.edge_subgraph(tour_edges), pos=my_pos, with_labels=True, node_color=node_colors, node_size=node_sizes, edgelist=tour_edges, edge_color='green', width=2.0)
+    # Visualizing the graph
+    #nx.draw(G, pos=my_pos, node_color=node_colors, node_size=node_sizes, with_labels=True)
+    #nx.draw_networkx_edges(G, pos=my_pos, edgelist=tour_edges, edge_color="green", width=2)
+    nx.draw(G, pos=my_pos, ax=ax,node_color=node_colors, node_size=node_sizes, edgelist=tour_edges, edge_color='green', width=2.0, with_labels=True)
+
+
+    # Adding the count of nodes and edges as a title to the plot
+    ax.set_title(f'Graph with {num_nodes} nodes and {num_edges} edges')
     plt.show()
 
